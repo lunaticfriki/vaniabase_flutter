67e7b10ce56ad1e8915cb5c474c607996a4c2606
ovenor11 <ovenor11@proton.me>
2026-02-24 14:08:29 +0100

    aboud screen

--- lib/presentation/screens/about_screen.dart
+++ lib/presentation/screens/about_screen.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/gestures.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/material.dart';
 import '../widgets/main_drawer.dart';
 
@@ -15,10 +17,63 @@ class _AboutView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
     return Scaffold(
       appBar: AppBar(title: const Text('About')),
       drawer: const MainDrawer(),
-      body: const Center(child: Text('About Screen')),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ShaderMask(
+                shaderCallback: (bounds) => const LinearGradient(
+                  colors: [Color(0xFFFF00FF), Color(0xFFFFFF00)],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                ).createShader(bounds),
+                child: Text(
+                  'Vaniabase is a free and open-source application for managing your items.',
+                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Version 1.0.0',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              RichText(
+                text: TextSpan(
+                  text: 'Author: ',
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: [
+                    TextSpan(
+                      text: '@lunaticfriki',
+                      style: Theme.of(
+                        context,
+                      ).textTheme.titleMedium?.copyWith(color: colors.tertiary),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () => launchUrl(
+                          Uri.parse('https://github.com/lunaticfriki'),
+                        ),
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                'Year: ${DateTime.now().year}',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
